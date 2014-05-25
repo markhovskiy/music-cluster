ef2af7d16bab4c6c78ea2ac8c6f68005787570cb
markhovskiy <olexiy.markhovsky@gmail.com>
2014-05-25 23:13:45 +0200

    add some colorcoding

--- list.py
+++ list.py
@@ -7,19 +7,33 @@ if not os.path.exists(root_dir):
 
 depth = None if len(sys.argv) is 1 else int(sys.argv[1])
 
+class effects:
+  end = '\033[0m'
+  bold = '\033[1m'
+  colors = {
+    'grey': '\033[90m',
+    'red': '\033[91m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'blue': '\033[94m',
+    'magenta': '\033[95m',
+    'cyan': '\033[96m',
+  }
+
 def list(dir_path, offset = 0, depth = None):
   if depth is 0:
     return
 
   for subfile_name in os.listdir(dir_path):
-    subdir_path = os.path.join(dir_path, subfile_name)
+    subfile_path = os.path.join(dir_path, subfile_name)
+    is_subfile_dir = os.path.isdir(subfile_path)
 
     for i in range(0, offset):
       print('  ', end='')
 
-    print(subfile_name)
+    print((effects.bold if is_subfile_dir else effects.colors['grey']) + subfile_name + effects.end)
 
-    if os.path.isdir(subdir_path):
-      list(subdir_path, offset + 1, None if depth is None else (depth - 1))
+    if is_subfile_dir:
+      list(subfile_path, offset + 1, None if depth is None else (depth - 1))
 
 list(root_dir, 0, depth)
