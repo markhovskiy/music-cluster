3f290321e1cdc93a5976f6d1bee11fdd3ee582f9
markhovskiy <olexiy.markhovsky@gmail.com>
2014-07-13 13:37:40 +0200

    derive the 'Lister' class

--- list.py
+++ list.py
@@ -1,9 +1,7 @@
-from __future__ import print_function
 import sys
 import os
 import argparse
-from mutagen.mp3 import MP3
-import effects
+from lister import Lister
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--color', action='store_true',
@@ -20,42 +18,5 @@ root_dir = os.path.abspath(args.path) if args.path else os.getcwd()
 if not os.path.exists(root_dir):
   sys.exit('Path not found: ' + root_dir)
 
-def get_file_ext(file_name):
-  return os.path.splitext(file_name)[1][1:].lower()
-
-def print_file_tags(file_path, separator = ' | '):
-  audio = MP3(file_path)
-
-  # (artist, year, album, title)
-  for frame in ('TPE1', 'TDRC', 'TALB', 'TIT2'):
-    if frame in audio:
-      print(audio[frame].text[0], end=separator)
-
-def print_file_data(file_path, file_name, is_subfile_dir):
-  file_ext = get_file_ext(file_name)
-
-  if args.tags and file_ext.lower() == 'mp3':
-    print_file_tags(file_path)
-
-  if not args.color:
-    print(file_name)
-    return
-
-  effect = 'bold' if is_subfile_dir else effects.get_ext_color(file_ext)
-  effects.print_with_effect(file_name, effect)
-
-def list(dir_path, offset = 0, depth = None):
-  if depth == 0:
-    return
-
-  for subfile_name in os.listdir(dir_path):
-    subfile_path = os.path.join(dir_path, subfile_name)
-    is_subfile_dir = os.path.isdir(subfile_path)
-
-    print('  ' * offset, end='')
-    print_file_data(subfile_path, subfile_name, is_subfile_dir)
-
-    if is_subfile_dir:
-      list(subfile_path, offset + 1, None if depth is None else (depth - 1))
-
-list(root_dir, 0, args.depth)
+lister = Lister(args.color, args.tags)
+lister.list(root_dir, 0, args.depth)
---
+++ lister.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import os
+from mutagen.mp3 import MP3
+import effects
+
+class Lister:
+  # number of spaces to indent (marking nesting)
+  offset_width = 2
+
+  # see http://id3.org/id3v2.4.0-frames
+  tag_frames = (
+    'TPE1', # artist
+    'TDRC', # year
+    'TALB', # album
+    'TIT2'  # title
+  )
+
+  tag_separator = ' | '
+
+  def __init__(self, to_color, to_print_tags):
+    self.to_color = to_color
+    self.to_print_tags = to_print_tags
+
+  def get_file_ext(self, file_name):
+    return os.path.splitext(file_name)[1][1:].lower()
+
+  def print_file_tags(self, file_path):
+    audio = MP3(file_path)
+
+    for frame in self.tag_frames:
+      if frame in audio:
+        print(audio[frame].text[0], end=self.tag_separator)
+
+  def print_file_data(self, file_path, file_name, is_dir):
+    file_ext = self.get_file_ext(file_name)
+
+    if self.to_print_tags and file_ext.lower() == 'mp3':
+      self.print_file_tags(file_path)
+
+    if not self.to_color:
+      print(file_name)
+      return
+
+    effect = 'bold' if is_dir else effects.get_ext_color(file_ext)
+    effects.print_with_effect(file_name, effect)
+
+  def list(self, dir_path, offset=0, depth=None):
+    """Print file/folder data recursively"""
+    if depth == 0:
+      return
+
+    for file_name in os.listdir(dir_path):
+      file_path = os.path.join(dir_path, file_name)
+      is_dir = os.path.isdir(file_path)
+
+      print(' ' * self.offset_width * offset, end='')
+      self.print_file_data(file_path, file_name, is_dir)
+
+      if is_dir:
+        self.list(file_path, offset + 1, None if depth is None else (depth - 1))
